cad1acef2f48aaa4252f12d97080d0d2d8d545a4
Lemenn <mrlmens01001@gmail.com>
2023-11-14 12:06:14 +0700

    Completed Admin loginpage

---
+++ app/src/main/java/com/kelompok8/finalproject2/TAMBAH_STAF.java
@@ -0,0 +1,16 @@
+package com.kelompok8.finalproject2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class TAMBAH_STAF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tambah_staff);
+    }
+}
+
---
+++ app/src/main/java/com/kelompok8/finalproject2/TAMBAH_STOK.java
@@ -0,0 +1,14 @@
+package com.kelompok8.finalproject2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TAMBAH_STOK extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_stok);
+    }
+}
